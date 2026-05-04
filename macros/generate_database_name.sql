687e83434e36cc590d5297f319b3156c4b52ab3c
Javier Almirón <javieralmiron@outlook.com>
2026-05-04 06:41:39 +0000

    Modificacion: generate_database_name.sql

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,4 +1,6 @@
---macros/generate_database_name.sql--nombre EXACTO obligatorio
+--macros/generate_database_name.sql
+--nombre EXACTO obligatorio
+
 {% macro generate_database_name(custom_database_name, node) -%}
     {%- if custom_database_name is not none -%}
         {{ custom_database_name | trim }}
@@ -6,3 +8,16 @@
         {{ target.database | trim }}
     {%- endif -%}
 {%- endmacro %}
+
+
+--macros/generate_schema_name.sql
+--nombre EXACTO obligatorio
+
+--{% macro generate_schema_name(custom_schema_name, node) -%}
+--    {%- set default_schema = target.schema -%}
+--    {%- if custom_schema_name is not none -%}
+--        {{ custom_schema_name | trim }}
+--    {%- else -%}
+--        {{ default_schema }}
+--    {%- endif -%}
+--{%- endmacro %}
